da3b46cab88186bdd0790698b16b25b899477bbb
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-05-08 16:52:55 +0100

    Adding copyright footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  
+  return (
+    <div className='text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200'>
+      <h1>&copy;{new Date().getFullYear()} Goggl, Inc.</h1>
+    </div>
+  );
+
 };
 
 export default Footer;
